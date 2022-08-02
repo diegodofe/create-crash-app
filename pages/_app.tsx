109518d99fd069a8f93024ebf37405cd90a756e4
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-08-01 20:23:42 -0400

    Layout content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,39 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app'
+import { ReactElement } from 'react'
 import '../styles/globals.css'
 
+function Layout({ children }: { children: ReactElement }) {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '8px',
+      }}
+    >
+      <div
+        style={{
+          padding: '8px',
+          borderRadius: '4px',
+          backgroundColor: 'red',
+          maxWidth: '720px',
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/cats.tsx
+++ pages/cats.tsx
@@ -12,17 +12,29 @@ export default function Cats({ cats }: { cats: Cat[] }) {
   return (
     <div>
       <h1>Cats</h1>
-      {cats?.map((cat) => (
-        <div key={cat.id}>
-          <h5>{cat.name}</h5>
-          <Image
-            alt={`${cat.name}-image`}
-            src={cat.image}
-            width={300}
-            height={300}
-          />
-        </div>
-      ))}
+
+      <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap' }}>
+        {cats?.map((cat) => (
+          <div
+            key={cat.id}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flex: '1',
+              minWidth: '240px',
+            }}
+          >
+            <p>{cat.name}</p>
+            <Image
+              alt={`${cat.name}-image`}
+              src={cat.image}
+              width={300}
+              height={300}
+              objectFit='cover'
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
