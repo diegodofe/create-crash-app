55c4b01cd84d50de215d0c3ace49dbac9c027e2b
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-10-30 19:31:07 -0400

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
+        <meta charSet='UTF-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>Create Crash App</title>
       </head>
       <body>
