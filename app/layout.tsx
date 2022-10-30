3ee08b01236283a875361064df612148103e0dbe
diegodofe <diego.dorantes-ferreira@mail.mcgill.ca>
2022-10-30 19:40:42 -0400

    Add more meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ export default function RootLayout({
         <meta charSet='UTF-8' />
         <meta http-equiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='robots' content='follow, index' />
+        <meta
+          name='description'
+          content='Scaffold a starter project using create-crash-app'
+        />
         <title>Create Crash App</title>
       </head>
       <body>
